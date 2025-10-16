d51253ed090c5c9290ac394c19c8416c0fe3ea07
vishwajeetsri <vs0397297@gmail.com>
2025-10-16 18:39:13 +0530

    Accepting/Rejecting connection request

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -2,35 +2,51 @@ import axios from "axios";
 import React, { useEffect } from "react";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
-import { addRequests } from "../utils/requestSlice";
+import { addRequests, removeRequest } from "../utils/requestSlice";
 
 const Requests = () => {
-    const requests = useSelector((store) => store.requests)
-    const dispatch = useDispatch()
+  const requests = useSelector((store) => store.requests);
+  const dispatch = useDispatch();
+
+  const reviewRequest = async (status, _id) => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/request/review/" + status + "/" + _id,
+        {},
+        { withCredentials: true }
+      );
+      dispatch(removeRequest(_id))
+    } catch (err) {}
+  };
+
   const fetchRequest = async () => {
     try {
       const res = await axios.get(BASE_URL + "/user/request/received", {
         withCredentials: true,
       });
-      
-      dispatch(addRequests(res.data.data))
+
+      dispatch(addRequests(res.data.data));
     } catch (err) {}
   };
 
   useEffect(() => {
-     fetchRequest()
-  },[])
+    fetchRequest();
+  }, []);
 
- if (!requests) return;
-  if (requests.length === 0) return <h1>No Requests Found</h1>;
+  if (!requests) return;
+  if (requests.length === 0) return <h1 className="ml-[40%] my-10">No Requests Found</h1>;
 
   return (
     <div className=" text-center my-10">
       <h1 className="text-bold text-white text-3xl">Connections Requests</h1>
       {requests.map((request) => {
-        const { _id, firstName, lastName, age, gender, photoUrl, about } = request.fromUserId;
+        const { _id, firstName, lastName, age, gender, photoUrl, about } =
+          request.fromUserId;
         return (
-          <div key={_id} className="flex justify-between items-center m-4 p-4 rounded-lg bg-base-300 w-[35%] mx-auto">
+          <div
+            key={_id}
+            className="flex justify-between items-center m-4 p-4 rounded-lg bg-base-300 w-[35%] mx-auto"
+          >
             <div>
               <img
                 className="w-20 h-20 rounded-full"
@@ -39,14 +55,24 @@ const Requests = () => {
               />
             </div>
             <div className="text-left mx-4">
-               <h2 className="font-bold text-xl">
+              <h2 className="font-bold text-xl">
                 {firstName + " " + lastName}
-               </h2>
-               {age && gender && <p>{age + " " +  gender}</p>}
-               <p>{about}</p>
+              </h2>
+              {age && gender && <p>{age + " " + gender}</p>}
+              <p>{about}</p>
             </div>
-            <button className="btn btn-active btn-primary mx-2">Reject</button>
-            <button className="btn btn-active btn-secondary mx-2">Accept</button>
+            <button
+              onClick={() => reviewRequest("rejected", request._id)}
+              className="btn btn-active btn-primary mx-2"
+            >
+              Reject
+            </button>
+            <button
+              onClick={() => reviewRequest("accepted", request._id)}
+              className="btn btn-active btn-secondary mx-2"
+            >
+              Accept
+            </button>
           </div>
         );
       })}
--- src/utils/requestSlice.js
+++ src/utils/requestSlice.js
@@ -4,9 +4,13 @@ const requestSlice = createSlice({
     name: "requests",
     initialState: [],
     reducers: {
-        addRequests: (state, action) => action.payload
+        addRequests: (state, action) => action.payload,
+        removeRequest: (state, action) => {
+           const newArray = state.filter((r) => r._id !== action.payload)
+           return newArray
+        }
     }
 })
 
-export const { addRequests } = requestSlice.actions
+export const { addRequests, removeRequest } = requestSlice.actions
 export default requestSlice.reducer 
