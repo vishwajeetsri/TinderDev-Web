c592b3da46454bf4a3ea5d6194954ea0f600636e
vishwajeetsri <vs0397297@gmail.com>
2026-05-21 17:05:54 +0530

    socket refresh

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,73 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { createSocketConnection } from '../utils/socket'
-import { useSelector } from 'react-redux'
+{/* Messages */}
+<div className="flex-1 overflow-y-auto px-4 py-6 space-y-4">
+  {messages.map((msg, index) => {
 
-const Chat = () => {
-    
-    const {targetUserId} = useParams()
-    const [messages, setMessages] = useState([])
-    const [newMessage, setNewMessage] = useState("")
-    const user  = useSelector((store) => store.user);
-    const userId = user?._id;
-    
-    useEffect(()=> {
-      if(!userId){
-        return;
-      }
-      const socket = createSocketConnection();
-      //As soon as the page loaded, the socket connection is made and joinChat event is emitted
-      socket.emit("joinChat", {firstName:user.firstName,userId, targetUserId})
+    const isMyMessage = msg.senderId === userId;
 
-      socket.on("messageReceived", ({firstName, text})=> {
-        console.log(firstName + " : " + text)
-        setMessages((messages) => [...messages, {firstName, text}]);
-      })
+    return (
+      <div
+        key={index}
+        className={`flex ${
+          isMyMessage ? "justify-end" : "justify-start"
+        }`}
+      >
+        <div
+          className={`
+            px-4 py-3 rounded-2xl text-sm shadow-lg
+            break-words w-fit max-w-[60%]
+            ${
+              isMyMessage
+                ? "bg-pink-500 text-white rounded-br-sm"
+                : "bg-gray-800 text-gray-100 rounded-bl-sm"
+            }
+          `}
+        >
 
-      return () => {
-        socket.disconnect();
-      };
+          {/* Name */}
+          <p
+            className={`text-xs mb-1 ${
+              isMyMessage
+                ? "text-pink-200 text-right"
+                : "text-cyan-300 text-left"
+            }`}
+          >
+            {msg.firstName}
+          </p>
 
-    }, [userId, targetUserId]);
-    
-    const sendMessage = () => {
-      const socket = createSocketConnection();
-      socket.emit("sendMessage", {
-        firstName: user.firstName ,
-        userId,
-        targetUserId,
-        text: newMessage
-      });
-      setNewMessage("")
-    };
-
-  return (
-    <div className='w-3/4 mx-auto rounded-xl border border-gray-600 m-5 h-[70vh] flex flex-col'>
-      <h1 className='p-5 border-b border-gray-600'>Chat</h1>
-      <div className='flex-1 overflow-scroll p-5'>
-        {messages.map((msg, index) => {
-  return (
-     <div key={index}>
-      <div className="chat-header">
-        {msg.firstName}
+          {/* Text */}
+          <p>{msg.text}</p>
+        </div>
       </div>
-         <div className="chat-bubble">
-              {msg.text}
-          </div>
-
-      </div>
-  )
-})}
-      </div>
-      <div className='p-5 border-t border-gray-600 flex items-center gap-2'>
-          <input value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value)}
-
-           type='text' className='flex-1 border border-gray-500 text-white rounded p-2'/>
-           <button onClick={sendMessage} className='btn btn-secondary'>Send</button>
-      </div>
-    </div>
-  )
-}
-
-export default Chat
+    );
+  })}
+</div>
